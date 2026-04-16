c11b9b714aac5c41d8be2911503c9d612f1a7aa9
mervekindan <mervekanmaz10@gmail.com>
2026-04-16 02:14:32 +0200

    Modify EventList component

--- app/src/components/EventList/EventList.jsx
+++ app/src/components/EventList/EventList.jsx
@@ -1,30 +1,20 @@
 import events from "../../data/events.js";
+import { EventCard } from "../EventCard/EventCard.jsx";
+import "./EventList.css";
 
 // TODO: split each event below into its own EventCard component
 // TODO: add a "Buy ticket" button to each event card
 // TODO: replace the mock data import with a fetch call to GET /events
 
 export default function EventList() {
-  return (
-    <ul>
-      {events.map((event) => (
-        <li key={event.id}>
-          <h2>{event.name}</h2>
-          <p>
-            {event.date} at {event.time}
-          </p>
-          <p>
-            {event.venue}, {event.city}
-          </p>
-          <p>{event.category}</p>
-          <p>{event.price === 0 ? "Free" : `€${event.price}`}</p>
-          <p>
-            {event.ticketsAvailable === 0
-              ? "Sold out"
-              : `${event.ticketsAvailable} tickets left`}
-          </p>
-        </li>
-      ))}
-    </ul>
-  );
+    return (
+        <section className="event-list">
+            <h1>Upcoming Events</h1>
+            <div className="event-grid">
+                {events.map((event) => (
+                    <EventCard key={event.id} event={event} />
+                ))}
+            </div>
+        </section>
+    );
 }
