a2ebf0f237e8c3a03090d835ce9f061e394b845f
mervekindan <mervekanmaz10@gmail.com>
2026-04-30 01:11:10 +0200

    Add search option and refactor category selector

--- app/src/components/EventList/EventList.jsx
+++ app/src/components/EventList/EventList.jsx
@@ -10,31 +10,69 @@ export default function EventList({ search, category }) {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(`${BASE_URL}/events`)
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error("Failed to fetch events");
-                }
-                return res.json();
-            })
-            .then((data) => {
-                setEvents(data);
-                setLoading(false);
-            })
-            .catch((err) => {
-                setError(err.message);
-                setLoading(false);
-            });
-    }, []);
+        const timeout = setTimeout(() => {
+            const query = new URLSearchParams();
+
+            if (search) query.append("q", search);
+            if (category !== "all") query.append("category_like", category);
+
+            setLoading(true);
+            setError(null);
+
+            fetch(`${BASE_URL}/events?${query.toString()}`)
+                .then((res) => {
+                    if (!res.ok) {
+                        throw new Error("Failed to fetch events");
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    setEvents(data);
+                    setLoading(false);
+                })
+                .catch((err) => {
+                    setError(err.message);
+                    setLoading(false);
+                });
+        }, 300);
+
+        return () => clearTimeout(timeout);
+    }, [category, search]);
 
     if (loading) return <p>Loading events...</p>;
     if (error) return <p>Error: {error}</p>;
 
     return (
-        <div className="event-grid">
-            {events.map((event) => (
-                <EventCard key={event.id} event={event} />
-            ))}
-        </div>
+        <section className="event-list">
+            <h2>
+                {" "}
+                {events.length === 0
+                    ? "No events found"
+                    : `${events.length} event${events.length > 1 ? "s" : ""} found`}
+            </h2>
+            {events.length === 0 ? (
+                <p>
+                    No events found
+                    {search && (
+                        <>
+                            {" "}
+                            for "<strong>{search}</strong>"
+                        </>
+                    )}
+                    {category !== "all" && (
+                        <>
+                            {" "}
+                            in category "<strong>{category}</strong>"
+                        </>
+                    )}
+                </p>
+            ) : (
+                <div className="event-grid">
+                    {events.map((event) => (
+                        <EventCard key={event.id} event={event} />
+                    ))}
+                </div>
+            )}
+        </section>
     );
 }
--- app/src/components/HomePage/HomePage.jsx
+++ app/src/components/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import EventDetail from "../EventDetail/EventDetail";
 import EventList from "../EventList/EventList";
 import "./HomePage.css";
@@ -7,13 +7,40 @@ import events from "../../data/events";
 // Feel free to replace the content of this component with your own
 function HomePage() {
     const [category, setCategory] = useState("all");
+    const [search, setSearch] = useState("");
 
     return (
         <div className="home-content">
-            <EventList category={category} setCategory={setCategory} />
+            <div className="home-header">
+                <h1>Upcoming Events</h1>
+                <div className="filtration">
+                    <input
+                        className="search-input"
+                        type="text"
+                        placeholder="Search events..."
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+
+                    <select
+                        className="filter-categories"
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                    >
+                        <option value="all">All Categories</option>
+                        <option value="conference">Conference</option>
+                        <option value="hackathon">Hackathon</option>
+                        <option value="workshop">Workshop</option>
+                        <option value="meetup">Meetup</option>
+                        <option value="bootcamp">Bootcamp</option>
+                    </select>
+                </div>
+            </div>
+
+            <EventList search={search} category={category} />
             <EventDetail event={events[2]} />
         </div>
     );
 }
-
+console.log(category);
 export default HomePage;
